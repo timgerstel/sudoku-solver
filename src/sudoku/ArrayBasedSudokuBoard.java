9d5a99f7bb286410c3424aa32d633a472269fbd3
Tim Gerstel <tgerstel@umass.edu>
2017-06-05 18:19:04 -0400

    Created class for generation of sudoku puzzles

---
+++ src/sudoku/ArrayBasedSudokuBoard.java
@@ -0,0 +1,18 @@
+package sudoku;
+
+/**
+ * Created by Tim on 6/5/2017.
+ */
+public class ArrayBasedSudokuBoard {
+
+    //Generate 9x9 Sudoku puzzle
+    public ArrayBasedSudokuBoard(){
+
+    }
+
+    //Generate nxn Sudoku puzzle
+    public ArrayBasedSudokuBoard(int n){
+
+    }
+
+}
---
+++ src/sudoku/SudokuSolver.java
@@ -0,0 +1,7 @@
+package sudoku;
+
+/**
+ * Created by Tim on 6/5/2017.
+ */
+public class SudokuSolver {
+}
