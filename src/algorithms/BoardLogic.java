1184c60b2094f98cb5a24c64d841b895ef9a2f88
Timothy Gerstel <tgerstel@umass.edu>
2017-06-07 23:21:40 -0400

    New boards are now initialized with a blank board.  Fixed dimensions of board based on input.  Added difficulty argument.

--- src/algorithms/BoardLogic.java
+++ src/algorithms/BoardLogic.java
@@ -2,19 +2,33 @@ package algorithms;
 
 import structures.ArrayBasedBlock;
 
-/**
- * Created by Tim on 6/5/2017.
- */
 public class BoardLogic {
     
+    private static int BLOCK_SIZE;
+    
     //Generates a new blank board
-    public static ArrayBasedBlock[][] newBoard(int n){
-        ArrayBasedBlock[][] board = new ArrayBasedBlock[n][n];
+    //Args: block size = sqrt(n)
+    //Returns blank sudoku board
+    private static ArrayBasedBlock[][] blankBoard(double n){
+        BLOCK_SIZE = (int) Math.sqrt(n);
+        ArrayBasedBlock[][] board = new ArrayBasedBlock[BLOCK_SIZE][BLOCK_SIZE];
         for(ArrayBasedBlock[] rows : board){
             for(ArrayBasedBlock b : rows){
-                b = new ArrayBasedBlock(n);
+                b = new ArrayBasedBlock(BLOCK_SIZE);
             }
         }
+        return board;
+    }
+    
+    public static ArrayBasedBlock[][] newBoard(int difficulty){
+        ArrayBasedBlock[][] board = blankBoard(9);
+        return board;
+    }
+    
+    //Note: make this generate a random, completely correct board then remove numbers based on a difficulty
+    public static ArrayBasedBlock[][] newBoard(int n, int difficulty){
+        ArrayBasedBlock[][] board = blankBoard(n);
+        
         return board;
     }
 }
