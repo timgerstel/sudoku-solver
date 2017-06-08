66225955d3df7205082beab21e4d34071842ef5c
Timothy Gerstel <tgerstel@umass.edu>
2017-06-08 00:16:12 -0400

    Added block size variable and getter

--- src/structures/ArrayBasedBlock.java
+++ src/structures/ArrayBasedBlock.java
@@ -3,18 +3,19 @@ package structures;
 public class ArrayBasedBlock {
 
     private int[][] block;
-    
-    //Initialize a new 3 x 3 block as the default
-    public ArrayBasedBlock(){
-        block = new int[3][3];
-    }
+    private int block_size;
     
     //Initialize a new n x n block.  Board size would be n^2 x n^2
     public ArrayBasedBlock(int n){
         block = new int[n][n];
+        block_size = n;
     }
 
     public int[][] getBlock(){
         return block;
+    }
+    
+    public int getBlockSize(){
+        return block_size;
     }
 }
