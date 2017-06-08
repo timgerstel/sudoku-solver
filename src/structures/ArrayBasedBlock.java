f31565414127a2a909086855f297fbe6cb0af6fb
Timothy Gerstel <tgerstel@umass.edu>
2017-06-07 23:25:05 -0400

    Commented Constructors

--- src/structures/ArrayBasedBlock.java
+++ src/structures/ArrayBasedBlock.java
@@ -1,16 +1,15 @@
 package structures;
 
-/**
- * Created by Tim on 6/5/2017.
- */
 public class ArrayBasedBlock {
 
     private int[][] block;
-
+    
+    //Initialize a new 3 x 3 block as the default
     public ArrayBasedBlock(){
         block = new int[3][3];
     }
-
+    
+    //Initialize a new n x n block.  Board size would be n^2 x n^2
     public ArrayBasedBlock(int n){
         block = new int[n][n];
     }
