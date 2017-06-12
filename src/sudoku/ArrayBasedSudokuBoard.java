a2007e2dbe8b22549783e55a0cadd34a05ebb3de
Tim <tgerstel@umass.edu>
2017-06-11 21:18:34 -0400

    Initializing the board as an ArrayBasedBlock matrix was a poor decision.  Board is now initialized as int matrix.  Simplified constructors.  Moved isFilled and fillBoard.

--- src/sudoku/ArrayBasedSudokuBoard.java
+++ src/sudoku/ArrayBasedSudokuBoard.java
@@ -11,56 +11,33 @@ import java.util.stream.IntStream;
  */
 public class ArrayBasedSudokuBoard {
     
-    private ArrayBasedBlock[][] board;
     //Matrix to store all numbers played in the game, i.e. a number played block row 1 col 3 would be at position matrix index [0][8]
-    private int[][] positionMatrix;
+    private int[][] board;
     private final int DEFAULT_SIZE = 9;
     private int BLOCK_SIZE;
 
     //Default constructor
     public ArrayBasedSudokuBoard(){
         //Generate 9 x 9 sudoku board with difficult 1
-       board = BoardLogic.newBoard(1);
-       positionMatrix = new int[DEFAULT_SIZE][DEFAULT_SIZE];
-       BLOCK_SIZE = board[0][0].getBlockSize();
+       board = BoardLogic.newBoard(board,1);
+       BLOCK_SIZE = 3;
     }
 
     //Generate nxn Sudoku puzzle
     public ArrayBasedSudokuBoard(int n, int difficulty){
-        board = BoardLogic.newBoard(n, difficulty);
-        positionMatrix = new int[n][n];
-        BLOCK_SIZE = board[0][0].getBlockSize();
-    }
-    
-    public boolean isFilled(){
-        for(int[] row : positionMatrix){
-            if(IntStream.of(row).anyMatch(x -> x == 0)){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public void fillBoard(){
-        while(!isFilled()){
-            int row = (int) Math.random() * (BLOCK_SIZE - 1);
-            int col = (int) Math.random() * (BLOCK_SIZE - 1);
-            for(int i = 0; i < BLOCK_SIZE; i++){
-                for(int j = 0; j < BLOCK_SIZE; j++){
-            
-                }
-            }
-        }
+        board = BoardLogic.newBoard(board, n, difficulty);
+        BLOCK_SIZE = BoardLogic.getBlockSize();
     }
     
     public int getBlockSize(){
         return BLOCK_SIZE;
     }
+    
 
     public void toConsole(){
-        for(int i = 0; i < positionMatrix.length; i++){
-            for(int j = 0; j < positionMatrix[0].length; j++){
-                System.out.print(positionMatrix[i][j]);
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                System.out.print(board[i][j]);
             }
             System.out.println(" ");
         }
