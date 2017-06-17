e9657caf06051b8a9f7ad56478b2add40693ebbe
Tim <tgerstel@umass.edu>
2017-06-17 15:59:31 -0400

    getValidMoves does not go into an infinite loop, however cannot correctly determine the best move after generating the first row.

--- src/algorithms/BoardLogic.java
+++ src/algorithms/BoardLogic.java
@@ -1,7 +1,5 @@
 package algorithms;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -32,23 +30,13 @@ public class BoardLogic {
         int[][] b = board;
         int MAX = b[0].length;
         for(int i = 0; i < b.length; i++) {
-            System.out.println("Current row: " + i);
+            System.out.println("CURRENT ROW: " + i);
             for (int j = 0; j < MAX; j++) {
+                System.out.println("\nCURRENT COL: " + j);
                 int[] moves = getValidMoves(b, i, j);
                 int value = (int) (Math.random() * (moves.length - 1)); //generate random value between 1 and max
-                System.out.println("RANGE: ");
                 b[i][j] = moves[value];
-//                boolean foundValid = false;
-//                while (!foundValid) { //check if move is valid
-//                    if(isValid(b, value, i, j)){
-//                        foundValid = true;
-//                    } else {
-//                        value = (int) (Math.random() * (MAX - 1)) + 1; //if move is invalid, generate new random value
-//                    }
-//                }
-                System.out.println("\nRandom value: " + value);
-                b[i][j] = value;
-                System.out.println("Array value: " + b[i][j]);
+                System.out.println("Random value: " + moves[value]);
             }
         }
         return b;
@@ -57,52 +45,33 @@ public class BoardLogic {
     private static int[] getValidMoves(int[][] board, int row, int col){
         int[] currentMoves = IntStream.rangeClosed(1, board[0].length).toArray();
         int blockRow = row / getBlockSize();
+        int blockCol = col / getBlockSize();
         List<Integer> moves = new ArrayList<>();
         //Check row and col
         for(int i = 0; i < board[0].length; i++) {
             if (board[row][i] != 0) {
-                currentMoves[board[row][i]] = 0;
+                currentMoves[board[row][i] - 1] = 0;
             }
             if(board[i][col] != 0){
-                currentMoves[board[i][col]] = 0;
+                currentMoves[board[i][col] - 1] = 0;
             }
         }
-        for(int i = blockRow; i < blockRow + getBlockSize(); i++){
-        
+        //Check surrounding block
+        for(int i = blockRow * getBlockSize(); i < (blockRow + 1) * getBlockSize(); i++){
+            for(int j = blockCol * getBlockSize(); j < (blockCol + 1) * getBlockSize(); j++){
+                if(board[i][j] != 0) {
+                    currentMoves[board[i][j] - 1] = 0;
+                }
+            }
         }
         for(int i = 0; i < currentMoves.length; i++){
             if(currentMoves[i] != 0){
                 moves.add(currentMoves[i]);
             }
         }
+        System.out.println("VALID MOVES for board[" + row + "][" + col + "]: " + moves.toString());
         return moves.stream().mapToInt(i->i).toArray();
     }
-    
-//    private static boolean isValid(int[][] board, int value, int row, int col){
-//        int blockRow = row % getBlockSize();
-//        int blockCol = col % getBlockSize();
-//        System.out.println("Check if surrounding block is valid");
-//        for(int i = blockRow; i < blockRow + getBlockSize(); i++){ //check if block is valid
-//            for(int j = 0; j < blockCol; j++){
-//                if(board[i][j] == value){
-//                    return false;
-//                }
-//            }
-//        }
-//        System.out.println("Check if Row is Valid");
-//        for(int val : board[row]){
-//            if(val == value){
-//                return false;
-//            }
-//        }
-//        System.out.println("Check if col is valid");
-//        for(int i = 0; i < board.length; i++){
-//            if(board[i][col] == value){
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
     
     public static int getBlockSize(){
         return (int) Math.sqrt(board[0].length);
