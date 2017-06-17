f9c870a7c591819cb0a4ce4104851566f5f70edf
Tim Gerstel <tgerstel@umass.edu>
2017-06-17 02:24:14 -0400

    getValidMoves generates a list of valid moves for a particular block

--- src/algorithms/BoardLogic.java
+++ src/algorithms/BoardLogic.java
@@ -1,5 +1,9 @@
 package algorithms;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class BoardLogic {
@@ -30,49 +34,75 @@ public class BoardLogic {
         for(int i = 0; i < b.length; i++) {
             System.out.println("Current row: " + i);
             for (int j = 0; j < MAX; j++) {
-                int value = (int) (Math.random() * (MAX - 1)) + 1; //generate random value between 1 and max
-                boolean foundValid = false;
-                while (!foundValid) { //check if move is valid
-                    if(isValid(b, value, i, j)){
-                        foundValid = true;
-                    } else {
-                        value = (int) (Math.random() * (MAX - 1)) + 1; //if move is invalid, generate new random value
-                    }
-                }
-                System.out.println("Value for row " + i + " column " + j + ": " + value);
+                int[] moves = getValidMoves(b, i, j);
+                int value = (int) (Math.random() * (moves.length - 1)); //generate random value between 1 and max
+                System.out.println("RANGE: ");
+                b[i][j] = moves[value];
+//                boolean foundValid = false;
+//                while (!foundValid) { //check if move is valid
+//                    if(isValid(b, value, i, j)){
+//                        foundValid = true;
+//                    } else {
+//                        value = (int) (Math.random() * (MAX - 1)) + 1; //if move is invalid, generate new random value
+//                    }
+//                }
+                System.out.println("\nRandom value: " + value);
                 b[i][j] = value;
                 System.out.println("Array value: " + b[i][j]);
             }
-            System.out.println("Next row");
         }
         return b;
     }
     
-    private static boolean isValid(int[][] board, int value, int row, int col){
-        int blockRow = row % getBlockSize();
-        int blockCol = col % getBlockSize();
-        System.out.println("Check if surrounding block is valid");
-        for(int i = blockRow; i < blockRow + getBlockSize(); i++){ //check if block is valid
-            for(int j = 0; j < blockCol; j++){
-                if(board[i][j] == value){
-                    return false;
-                }
+    private static int[] getValidMoves(int[][] board, int row, int col){
+        int[] currentMoves = IntStream.rangeClosed(1, board[0].length).toArray();
+        int blockRow = row / getBlockSize();
+        List<Integer> moves = new ArrayList<>();
+        //Check row and col
+        for(int i = 0; i < board[0].length; i++) {
+            if (board[row][i] != 0) {
+                currentMoves[board[row][i]] = 0;
+            }
+            if(board[i][col] != 0){
+                currentMoves[board[i][col]] = 0;
             }
         }
-        System.out.println("Check if Row is Valid");
-        for(int val : board[row]){
-            if(val == value){
-                return false;
+        for(int i = blockRow; i < blockRow + getBlockSize(); i++){
+        
+        }
+        for(int i = 0; i < currentMoves.length; i++){
+            if(currentMoves[i] != 0){
+                moves.add(currentMoves[i]);
             }
         }
-        System.out.println("Check if col is valid");
-        for(int i = 0; i < board.length; i++){
-            if(board[i][col] == value){
-                return false;
-            }
-        }
-        return true;
+        return moves.stream().mapToInt(i->i).toArray();
     }
+    
+//    private static boolean isValid(int[][] board, int value, int row, int col){
+//        int blockRow = row % getBlockSize();
+//        int blockCol = col % getBlockSize();
+//        System.out.println("Check if surrounding block is valid");
+//        for(int i = blockRow; i < blockRow + getBlockSize(); i++){ //check if block is valid
+//            for(int j = 0; j < blockCol; j++){
+//                if(board[i][j] == value){
+//                    return false;
+//                }
+//            }
+//        }
+//        System.out.println("Check if Row is Valid");
+//        for(int val : board[row]){
+//            if(val == value){
+//                return false;
+//            }
+//        }
+//        System.out.println("Check if col is valid");
+//        for(int i = 0; i < board.length; i++){
+//            if(board[i][col] == value){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
     
     public static int getBlockSize(){
         return (int) Math.sqrt(board[0].length);
